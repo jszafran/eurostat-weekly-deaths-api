4e94328a706bb08aaf47dbeaae775a31e09cf0ad
jszafran <jszafran.pv@gmail.com>
2023-05-08 08:44:24 +0200

    Adding func for inserting weekly deaths data into db.

--- db.go
+++ db.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// GetDB prepares a connection to SQLite database.
 func GetDB() (*sql.DB, error) {
 	var db *sql.DB
 	db, err := sql.Open("sqlite3", "./eurostat.db")
@@ -16,6 +17,7 @@ func GetDB() (*sql.DB, error) {
 	return db, err
 }
 
+// RecreateDB drops and creates a weekly_deaths table.
 func RecreateDB(db *sql.DB) error {
 	_, err := db.Exec("DROP TABLE IF EXISTS weekly_deaths")
 	if err != nil {
@@ -32,6 +34,44 @@ func RecreateDB(db *sql.DB) error {
 			country STRING
 		) 
 	`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func InsertWeeklyDeathsData(records []WeeklyDeathsRecord, db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(`
+		INSERT INTO weekly_deaths (week, year, deaths, age, sex, country)
+		VALUES (?, ?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	for _, r := range records {
+		_, err = stmt.Exec(
+			r.Week,
+			r.Year,
+			r.Deaths,
+			r.Age,
+			r.Sex,
+			r.Country,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = tx.Commit()
 	if err != nil {
 		return err
 	}
